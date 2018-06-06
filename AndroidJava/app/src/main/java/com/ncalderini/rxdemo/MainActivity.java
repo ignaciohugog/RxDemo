4b01eb001bca70035c3dd3fdf4a6c283140ede3b
ncalderini <calderini.nicolas@gmail.com>
2018-06-06 02:18:08 -0300

    Java solution

--- AndroidJava/app/src/main/java/com/ncalderini/rxdemo/MainActivity.java
+++ AndroidJava/app/src/main/java/com/ncalderini/rxdemo/MainActivity.java
@@ -2,6 +2,14 @@ package com.ncalderini.rxdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.jakewharton.rxbinding2.widget.RxTextView;
+
+import io.reactivex.Observable;
+import io.reactivex.functions.Function3;
+import io.reactivex.observers.DisposableObserver;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +17,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText firstNameTextField = findViewById(R.id.editText);
+        final EditText lastNameTextField = findViewById(R.id.editText2);
+        final EditText emailTextField = findViewById(R.id.editText3);
+        final Button submitButton = findViewById(R.id.button);
+
+        final Observable<CharSequence> observableName = RxTextView.textChanges(firstNameTextField);
+        final Observable<CharSequence> observableLastName = RxTextView.textChanges(lastNameTextField);
+        final Observable<CharSequence> observableEmail = RxTextView.textChanges(emailTextField);
+
+        Observable.combineLatest(observableName, observableLastName, observableEmail, new Function3<CharSequence, CharSequence, CharSequence, Boolean>() {
+            @Override
+            public Boolean apply(CharSequence firstName, CharSequence lastName, CharSequence email) {
+                return !firstName.toString().isEmpty() && !lastName.toString().isEmpty() && !email.toString().isEmpty();
+            }
+        }).subscribe(new DisposableObserver<Boolean>() {
+            @Override
+            public void onNext(Boolean enabled) {
+                submitButton.setEnabled(enabled);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
 
     }
 }
